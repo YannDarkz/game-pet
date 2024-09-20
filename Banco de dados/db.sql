f59b0b190bf8bd719659b4a51d958dc017674b00
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-09-20 13:07:51 -0300

    Finalizado banco de dados. Adicionados classes para controle de vida, nescessidades e pets.

--- Banco de dados/db.sql
+++ Banco de dados/db.sql
@@ -13,7 +13,6 @@ CREATE TABLE `petlove`.`usuarios`
     UNIQUE INDEX `Email_UNIQUE` (`Email`)
 );
 
-
 CREATE TABLE `petlove`.`authentication`
 (
     `id`         INT         NOT NULL AUTO_INCREMENT,
@@ -24,5 +23,45 @@ CREATE TABLE `petlove`.`authentication`
     CONSTRAINT `fk_usuario_auth`
         FOREIGN KEY (`id_usuario`)
             REFERENCES `petlove`.`usuarios` (`id`)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE `petlove`.`pets`
+(
+    `id`         INT         NOT NULL AUTO_INCREMENT,
+    `id_usuario` INT         NOT NULL,
+    `name`       VARCHAR(45) NOT NULL,
+    `age`        INT         NOT NULL,
+    `type`       VARCHAR(15) NOT NULL,
+    `condition`  TINYINT     NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_usuario_pet`
+        FOREIGN KEY (`id_usuario`)
+            REFERENCES `petlove`.`usuarios` (`id`)
+            ON DELETE CASCADE
+);
+CREATE TABLE `petlove`.`health`
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `id_pet` INT NOT NULL,
+    `life` INT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_pet_health`
+        FOREIGN KEY (`id_pet`)
+            REFERENCES `petlove`.`pets` (`id`)
+            ON DELETE CASCADE
+);
+
+
+CREATE TABLE `petlove`.`needs`
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `id_pet` INT NOT NULL,
+    `urine` INT NULL,
+    `faces` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_pet_health`
+        FOREIGN KEY (`id_pet`)
+            REFERENCES `petlove`.`pets` (`id`)
             ON DELETE CASCADE
 );
--- src/main/java/com/game/petlove/model/PetModel.java
+++ src/main/java/com/game/petlove/model/PetModel.java
@@ -6,44 +6,60 @@ public class PetModel {
     private int id;
     private String name;
     private int age;
-    private String pet = null;
+    private String pet ;
+    private boolean petStatus;
 
-    public PetModel(int id, String name, int age, String pet) {
+    public PetModel(int id, String name, int age, String pet, boolean petStatus) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.pet = pet;
-    }
-
-    @Override
-    public String toString() {
-        return "PetModel{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", pet=" + pet +
-                '}';
+        this.petStatus = petStatus;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
         return age;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getPet() {
         return pet;
     }
 
+    public void setPet(String pet) {
+        this.pet = pet;
+    }
+
+    public boolean isPetStatus() {
+        return petStatus;
+    }
+
+    public void setPetStatus(boolean petStatus) {
+        this.petStatus = petStatus;
+    }
+
     public static void main(String[] args) {
         PetsEnum animal = PetsEnum.animalCachorro;
-        PetModel pet = new PetModel(1, "Dudinha", 6, animal.getNome());
+        PetModel pet = new PetModel(1, "Dudinha", 6, animal.getNome(), false);
         System.out.println(pet);
     }
 }
