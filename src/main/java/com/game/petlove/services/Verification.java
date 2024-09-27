4d6bb7a09624516f1bca8d713b89b847ce343f4a
eduardo <eduardofreitasfr@gmail.com>
2024-09-27 12:55:11 -0300

    Adicionado funcionalidade para verificar campos

--- src/main/java/com/game/petlove/services/Verification.java
+++ src/main/java/com/game/petlove/services/Verification.java
@@ -1,25 +1,17 @@
-package com.game.petlove.model.service;
+package com.game.petlove.services;
 
 import com.game.petlove.model.ClienteModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@Service
 public class Verification {
     public boolean checkForEmptyFields(ClienteModel cliente){
         if(cliente.getName().isEmpty()  && cliente.getEmail().isEmpty() && cliente.getAge() != 0){
-            System.out.println("Estou com todos dados");
             return true;
         } else {
-            System.out.println("Está faltando dados");
             return false;
         }
-    }
-
-    public static void main(String[] args) {
-        ClienteModel cliente = new ClienteModel();
-        cliente.setName("Jose");
-        cliente.setEmail("sync@gmail.com");
-        cliente.setAge(1);
-
-        Verification verification = new Verification();
-        System.out.println(verification.checkForEmptyFields(cliente));
     }
 }
